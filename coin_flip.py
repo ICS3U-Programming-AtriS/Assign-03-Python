8459a043b6a94cf8ca983ad3bc7e9d904693f0c3
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-04-07 15:56:31 +0000

    Added coin flipping game, coin_flip.py

---
+++ coin_flip.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# Created By: Atri Sarker
+# Date: April 7, 2025
+# Coin Flipping game
+
+import util
+
+def main():
+    # Get user's guess for whether a coin will result in
+    # "HEADS" or "TAILS"
+    guess = util.option_input("Heads/Tails?", ["HEADS", "TAILS"])
+
+    if guess == "HEADS":
+        print("The coin was tails, unfortunately.")
+    # If guess is not "HEADS", it must be "TAILS"
+    else:
+        print("The coin was heads, unfortunately.")
+
+
+
+if __name__ == "__main__":
+    main()
